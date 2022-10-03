88f9cd88d9ae5e475e7b74a73824454768abb1e3
maribogl <maribogl@yandex.ru>
2022-10-03 16:42:35 +0300

    tasks 2,4,6,8

---
+++ task04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+int max = numberOne;
+if (numberTwo > max)
+{
+    max = numberTwo;
+}
+if (numberThree > max)
+{
+    max = numberThree;
+}
+Console.WriteLine($"Максимальное число => {max}");
---
+++ task08/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        if (counter % 2 == 0)
+        {
+            Console.Write($"{counter}, ");
+        }
+        counter++;
+    }
+}
+else Console.Write($"Задан некорректный числовой интервал (от 1 до {number})");
